1c056cbe954da31ad3af4d80a4a02c0d76359967
Uladzimir <2016vovik@gmail.com>
2024-04-06 17:07:30 +0300

    Added "add(T elem)" method

--- src/by/myCollections/MyLinkedList.java
+++ src/by/myCollections/MyLinkedList.java
@@ -1,12 +1,26 @@
 package by.myCollections;
 
-import java.util.LinkedList;
-
 public class MyLinkedList<T extends Comparable<T>> {
-    Node<T> node;
+    Node<T> first;
+    Node<T> last;
+    int size;
 
     public MyLinkedList() {
-        node = new Node<>(null, null, null);
+        first = new Node<>(null, null, null);
+        last = first;
+        size = 0;
+    }
+
+    public void add(T elem) {
+        if (first == null && last == null) {
+            first = new Node<>(null, elem, null);
+            last = first;
+        }
+        else if (first == last) {
+            last = new Node<>(first, elem, null);
+            first.next = last;
+        }
+        else last = new Node<>(last.prev, elem, null);
     }
 
     private static class Node<T> {
