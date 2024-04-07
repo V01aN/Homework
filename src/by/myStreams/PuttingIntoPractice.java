b4e3638388bfac7867958cb09547f80a6a78a120
Uladzimir <2016vovik@gmail.com>
2024-04-08 00:37:55 +0300

    Added stream for task 2

--- src/by/myStreams/PuttingIntoPractice.java
+++ src/by/myStreams/PuttingIntoPractice.java
@@ -1,8 +1,10 @@
 package by.myStreams;
 
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -25,34 +27,46 @@ public class PuttingIntoPractice {
                 new Transaction(alan, 2012, 950)
         );
         //task 1
+        System.out.println("Task 1:");
         transactions.stream()
                 .filter(x -> x.getYear() == 2011)
                 .sorted(Comparator.comparing(Transaction::getYear))
                 .forEach(System.out::println);
         //task 2
-
+        System.out.println("Task 2:");
+        Stream.of(raoul, mario, alan, brian)
+                        .map(Trader::getCity)
+                                .distinct()
+                                        .forEach(System.out::println);
         //task 3
+        System.out.println("Task 3:");
         Stream.of(raoul, mario, alan, brian)
                 .filter(x -> x.getCity() == "Cambridge")
                 .sorted(Comparator.comparing(Trader::getName))
                 .forEach(System.out::println);
         //task 4
+        System.out.println("Task 4:");
         Stream.of(raoul, mario, alan, brian)
                 .sorted(Comparator.comparing(Trader::getName))
                 .forEach(x -> System.out.print(x.getName() + " "));
         //task 5
         System.out.println();
+        System.out.println("Task 5:");
         System.out.println(Stream.of(raoul, mario, alan, brian)
                 .anyMatch(x -> x.getCity() == "Milan"));
         //task 6
+        System.out.println("Task 6:");
         System.out.println(transactions.stream()
                 .filter(x -> x.getTrader().getCity() == "Cambridge").
                 mapToInt(x -> x.getValue()).sum());
         //task 7
-        System.out.println((transactions.stream()
-                .max(Comparator.comparing(Transaction::getValue))
-                .get()));
+        System.out.println("Task 7:");
+        System.out.println(transactions.stream()
+                .map(Transaction::getValue)
+                .max(Comparator.naturalOrder())
+                .get());
         //task 8
+        System.out.println("Task 8:");
         System.out.println(transactions.stream()
                 .reduce(BinaryOperator.minBy(Comparator.comparing(Transaction::getValue)))
                 .get());
